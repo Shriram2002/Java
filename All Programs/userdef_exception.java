94bd8f5355f4590d4b15c388d57f1bea97ef46cc
Shriram2002 <68805824+Shriram2002@users.noreply.github.com>
2021-01-03 13:19:14 +0530

    User defined exception handling

---
+++ All Programs/userdef_exception.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class NegativeAmtException extends Exception
+{
+ String msg;
+ NegativeAmtException(String msg)
+ {
+ this.msg=msg;
+ }
+ public String toString()
+ {
+ return msg;
+ }
+}
+public class userdef_exception
+{
+ public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter Amount:");
+int a=s.nextInt();
+try
+{
+if(a<0)
+ {
+throw new NegativeAmtException("Invalid Amount");
+ }
+ System.out.println("Amount Deposited");
+ }
+catch(NegativeAmtException e)
+{
+ System.out.println(e);
+}
+}
+}
