d9970cb9daf8a4538ce061bd05622a7a6cf1aa82
Shriram2002 <68805824+Shriram2002@users.noreply.github.com>
2021-01-22 20:15:16 +0530

    Generic Function Implementation
    To find max, min

--- All Programs/Generic_Function.java
+++ All Programs/Generic_Function.java
@@ -3,10 +3,12 @@ public class Generic_Function
 public static <T extends Comparable<T>> T maximum(T x, T y, T z)
 {
 T max = x; 
-if(y.compareTo(max) > 0) {
+if(y.compareTo(max) > 0) 
+{
 max = y;
 }
-if(z.compareTo(max) > 0) {
+if(z.compareTo(max) > 0) 
+{ 
 max = z;
 }
 return max; 
